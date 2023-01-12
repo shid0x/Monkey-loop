434933a64a2336092bc58ec04cbe24debc1aaf21
ThomasL <thomas.leguilloudp@gmail.com>
2023-01-12 19:48:38 +0100

    loop complete

--- ruby/run.rb
+++ ruby/run.rb
@@ -12,8 +12,30 @@ youtube_url = config['youtube_url']
 # ouvrir un stream stream
 video_stream = open(youtube_url)
 
-# todo : boucle & password 
+# On ouvre la vidéo 
+
+fsevent = FSEvent.new
+fsevent.watch video_stream do
+  puts "Attention un babouin !"
+
+  system("afplay", video_stream.path)
 
 
+# Mot de passe ?
+  puts "Quel est le mot sacré ?"
+  entered_password = gets.chomp
+
+  # comparer mdp
+  if entered_password == password
+    # arrêt du loop
+    fsevent.stop
+    puts "Les singes ont été calmés "
+  else
+    puts "OUH OUH OUH OUH OUH ESSSAIE ENCORE."
+  end
+end
+
+# loop
+fsevent.run
 end
 
